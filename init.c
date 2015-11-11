ddc42eceebe0d80664d8eb414af04b30c98c1b32
Simon <simoncmilburn@gmail.com>
2015-11-11 12:56:37 -0500

    init.c

--- init.c
+++ init.c
@@ -8,6 +8,34 @@ void getKey(){
 
 }
 
-int main(){
+int main(int argc, char** argv){
+    FILE *bank, *atm;
+    char *atm_fn, *bank_fn    
+    atm_fn = ;    
+    bank_fn = ;
+    //if not single arg, usage err
+    if(argc != 2){
+        printf("Usage: init <filename>\n");
+        return 62;
+    }
 
-}
+    //init files already exist
+    if(already exists){
+        printf("Error: one of the files already exists\n");
+        return 63;
+    }    
+
+    //init failure
+    if((atm = fopen(atm_fn, "w")) == NULL || (bank = fopen(bank_fn, "w")) == NULL){
+        printf("Error creating initialization files\n");
+        return 64;
+    }
+    getKeys();
+    
+    //write keys to initfiles
+    //fwrite....
+
+
+    printf("Successfully initialized bank state\n");
+    return 0;   
+}   
